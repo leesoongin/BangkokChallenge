0c47dfae45341e5a33ecf07c972f17dafc0613d5
chad1994 <chad1994@skuniv.ac.kr>
2020-04-08 23:54:08 +0900

    TimelineFragment 구분

--- app/src/main/java/com/example/bangkokchallenge/timeline/TimelineFragment.kt
+++ app/src/main/java/com/example/bangkokchallenge/timeline/TimelineFragment.kt
@@ -2,75 +2,53 @@ package com.example.bangkokchallenge.timeline
 
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bangkokchallenge.R
-import com.example.bangkokchallenge.model.PostDTO
-import kotlinx.android.synthetic.main.fragment_timeline_view.view.*
-import kotlinx.android.synthetic.main.item_post.view.*
+import com.example.bangkokchallenge.model.TimeLineItem
 
 
-class TimelineViewFragment : Fragment() ,TimeLineContract.View {
+class TimelineFragment : Fragment(), TimeLineContract.View {
 
-    lateinit var contentList:ArrayList<PostDTO> // content에 들어갈 정보 list
     override lateinit var presenter: TimeLineContract.Presenter
+    private lateinit var interactor: TimeLineContract.TimeLineInteractor
+
+    private lateinit var recyclerView: RecyclerView
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view =LayoutInflater.from(activity).inflate(R.layout.fragment_timeline_view,container,false)
+        val view =
+            LayoutInflater.from(activity).inflate(R.layout.fragment_timeline, container, false)
 
-        contentList= arrayListOf(
-            PostDTO("profile","nickname",0,0,"des","date"),
-            PostDTO("profile","name2",2,3,"","postdate2")
-        )
+        initViews(view)
 
-        presenter=TimeLinePresenter(this)
-
-        view.timeline_recyclerview.adapter=TimelineViewRecyclerViewAdapter()
-        view.timeline_recyclerview.layoutManager=LinearLayoutManager(activity)
+        interactor = TimeLineInteractorImpl()
+        presenter = TimeLinePresenter(this@TimelineFragment, interactor)
+        presenter.requestTimeLineDataFromServer()
 
         return view
-    }//oncreate
-
-    override fun pushLike() {
-        Log.d("push","좋아요")
     }
 
-    override fun pushComment() {
-        Log.d("push","댓글")
+    private fun initViews(view: View) {
+        recyclerView = view.findViewById(R.id.timeline_recyclerview)
+        val layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        recyclerView.layoutManager = layoutManager
     }
 
-    override fun pushMoreview() {
-        Log.d("push","상세보기")
+    override fun setRecyclerViewData(responseData: List<TimeLineItem>) {
+        val adapter = TimeLineAdapter(responseData)
+        recyclerView.adapter = adapter
     }
 
-    inner class TimelineViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
-
-            return CostomViewHolder(view)
-        }
-
-        inner class CostomViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-        override fun getItemCount(): Int {
-            return contentList.size
-        }
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            var viewHolder = (holder as CostomViewHolder).itemView
-
-        }
+    override fun onResponseFailure(t: Throwable?) {
+        Toast.makeText(requireContext(), "데이터 불러오기에 실패 했습니다.", Toast.LENGTH_SHORT).show()
     }
-
-
 }
